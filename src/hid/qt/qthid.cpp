ba6da34a9b4bf83a4e4b38303cc704aba0495bfd
Dale May <dmay31@gmail.com>
2014-12-01 19:36:11 -0600

    Some Cleanup work
    Did some clean up work and working on getting the zoom to work
    around the mouse point.

--- src/hid/qt/qthid.cpp
+++ src/hid/qt/qthid.cpp
@@ -1,14 +1,14 @@
 #include "qthid.h"
 
 /*-----------------------------------------------
-  	  	  	     Variables
+                       Variables
  ----------------------------------------------*/
 QtHID*   QtHID::pMe;
 HID*     QtHID::pIntf;
 HID_DRAW QtHID::QtGraphics;
 
 /*-----------------------------------------------
-  	  	  	     Functions
+                       Functions
  ----------------------------------------------*/
 
 
@@ -16,6 +16,15 @@ HID_DRAW QtHID::QtGraphics;
 /* Constructor */
 QtHID::QtHID(QWidget *Parent)
 {
+/* Default Zoom Level */
+zoom = 1;
+tx = 0;
+ty = 0;
+
+/* Default Pixels Per X */
+ppx = 1 / zoom;
+
+
 setMouseTracking(true);
 qtGreen = QColor::fromRgbF(0,0,0,0);
 
@@ -27,6 +36,11 @@ QtHID::~QtHID()
 
 }
 
+
+void QtHID::initializeGL()
+{
+qglClearColor(qtGreen.dark());
+}
 /** @brief Instantiates a new HID object and registers it
  *         with the PCB framework.
  *
@@ -89,7 +103,7 @@ return gc;
  */
 void QtHID::parse_arguments( int *argc, char ***argv )
 {
-//TODO
+ghid_config_init();
 } /* parse_arguments() */
 
 /**
@@ -151,91 +165,368 @@ void QtHID::set_draw_xor( hidGC gc, int xor_ )
 //TODO
 }
 
+/**
+ * @Brief Draw a circular cap on a blunt end
+ *
+ * @param Width of the end
+ * @param X center location
+ * @param Y center location
+ * @param Angle of the blunt end
+ * @param Scale factor
+ */
+void QtHID::draw_cap (Coord width, Coord x, Coord y, Angle angle, double scale)
+{
+#define MIN_TRIANGLES_PER_CAP 3
+#define MAX_TRIANGLES_PER_CAP 90
+float last_capx, last_capy;
+float capx, capy;
+float radius = width / 2.;
+int slices;
+int i;
+
+/* Calculate the number of slices for this circle */
+slices = calc_slices (radius / scale, M_PI);
+
+/* Boundary Checking */
+if (slices < MIN_TRIANGLES_PER_CAP)
+    {
+    slices = MIN_TRIANGLES_PER_CAP;
+    }
+
+/* Boundary Checking */
+if (slices > MAX_TRIANGLES_PER_CAP)
+    {
+    slices = MAX_TRIANGLES_PER_CAP;
+    }
+
+/* Check the buffer is large enough */
+if( !check_triangle_space (&buffer, slices) )
+    {
+    /* Very bad error */
+    exit( 1 );
+    }
+
+/* Get the starting x,y coordinates */
+last_capx =  radius * cosf (angle * M_PI / 180.) + x;
+last_capy = -radius * sinf (angle * M_PI / 180.) + y;
+
+for (i = 0; i < slices; i++)
+    {
+  /* Get slice[i+1] x and y */
+    capx =  radius * cosf (angle * M_PI / 180. + ((float)(i + 1)) * M_PI / (float)slices) + x;
+    capy = -radius * sinf (angle * M_PI / 180. + ((float)(i + 1)) * M_PI / (float)slices) + y;
+
+    /* Add triangle to the buffer */
+    add_triangle (&buffer, last_capx, last_capy, capx, capy, x, y);
+
+    /* Store the last coordinate */
+    last_capx = capx;
+    last_capy = capy;
+    }
+} /* draw_cap() */
+
+bool QtHID::check_triangle_space( triangle_buffer *buffer, int count )
+{
+/* Local variables */
+bool ret = false;
+
+/* Check that count will fit if buffer is empty */
+if( count <= TRIANGLE_ARRAY_SIZE )
+    {
+    /* Flush buffer in more room is needed */
+    if( count > ( TRIANGLE_ARRAY_SIZE - buffer->triangle_count ) )
+        {
+        flush_triangles( buffer );
+        }
+    /* return no error */
+    ret = true;
+    }
+
+return( ret );
+}
+
+
 void QtHID::set_line_width( hidGC gc, Coord width )
 {
 gc->width = width;
 }
 
-void QtHID::flush_triangles(triangle_buffer* buffer)
-	{
-	 // if (buffer->triangle_count == 0)
-	 //   return;
+#define MAX_PIXELS_ARC_TO_CHORD 0.5
+#define MIN_SLICES 6
+int QtHID::calc_slices (float pix_radius, float sweep_angle )
+{
+  float slices;
 
+  if (pix_radius <= MAX_PIXELS_ARC_TO_CHORD)
+    return MIN_SLICES;
 
-      glEnableClientState (GL_VERTEX_ARRAY);
-      glVertexPointer (3, GL_FLOAT, 0,buffer->triangle_array);
-	  glDrawArrays (GL_TRIANGLES, 0, buffer->triangle_count * 3);
+  slices = sweep_angle / acosf (1 - MAX_PIXELS_ARC_TO_CHORD / pix_radius) / 2.;
+  return (int)ceilf (slices);
+}
 
-	  glDisableClientState (GL_VERTEX_ARRAY);
+#define MIN_SLICES_PER_ARC 6
+#define MAX_SLICES_PER_ARC 360
 
+/**
+ * @Brief Draw an arc with with set width, coord, and inner and outer diameter
+ *
+ * @param Width - Width of the Band around the arc
+ * @param X position for the center of the circle
+ * @parma Y position for the cneter of the circle
+ * @param Radius of the circle in the horizontal direction
+ * @param Radius of the circle in the vertical direction ( not used )
+ * @param Start Angle
+ * @param Delta Angle - The degrees the arc will sweep through
+ * @param Scale
+ *
+ * @return Void.
+*/
+void QtHID::draw_arc
+    (
+    Coord       width,           // Width of the band
+    Coord       x,               // x position, center of circle
+    Coord       y,               // y position, center of circle
+    Coord       rx,              // radius of arc, to the outside of the band
+    Coord       ry,              // Unused!
+    Angle       start_angle,     // Start angle
+    Angle       delta_angle,     // Degrees to sweep the arc
+    double      scale            // Current scale factor
+    )
+{
+/* Local Variables */
+float last_inner_x, last_inner_y;
+float last_outer_x, last_outer_y;
+float inner_x, inner_y;
+float outer_x, outer_y;
+float inner_r;
+float outer_r;
+float cos_ang, sin_ang;
+float start_angle_rad;
+float delta_angle_rad;
+float angle_incr_rad;
+int slices;
+int i;
+int hairline = 0;
 
+/* Check for hair line arc */
+if (width == 0.0)
+    {
+    hairline = 1;
+    }
 
-	  buffer->triangle_count = 0;
-	  buffer->coord_comp_count = 0;
-	}
+/* Sanity check the width against scale */
+if (width < scale)
+    {
+    width = scale;
+    }
+
+/* Calculate inner and outer radius */
+inner_r = rx - width / 2.;
+outer_r = rx + width / 2.;
+
+/* Covert negative delta angle */
+if (delta_angle < 0)
+    {
+    start_angle += delta_angle;
+    delta_angle = - delta_angle;
+    }
+
+/* Get starting and ending angle in Radians */
+start_angle_rad = start_angle * M_PI / 180.;
+delta_angle_rad = delta_angle * M_PI / 180.;
+
+slices = calc_slices ( outer_r / scale, delta_angle_rad);
+
+/* Boundary checking */
+if (slices < MIN_SLICES_PER_ARC)
+  slices = MIN_SLICES_PER_ARC;
+
+/* Boundary checking */
+if (slices > MAX_SLICES_PER_ARC)
+  slices = MAX_SLICES_PER_ARC;
+
+/* Check for room and flush if needed */
+if( !check_triangle_space (&buffer, 2 * slices) )
+    {
+    /* Very bad error */
+    exit( 1 );
+    }
+
+/* Radians per slice */
+angle_incr_rad = delta_angle_rad / (float)slices;
+
+/* Get the x/r and y/r ratios */
+cos_ang = cosf (start_angle_rad);
+sin_ang = sinf (start_angle_rad);
+
+/* Get X and Y positions for inner arc*/
+last_inner_x = -inner_r * cos_ang + x;
+last_inner_y = inner_r * sin_ang + y;
+
+/* Get X and Y positions for outer arc */
+last_outer_x = -outer_r * cos_ang + x;
+last_outer_y = outer_r * sin_ang + y;
+
+/* iterate over each slice, drawing a block with two triangles */
+for (i = 1; i <= slices; i++)
+    {
+    /* Get the x/r and y/r ratios for each 'spoke' */
+    cos_ang = cosf (start_angle_rad + ((float)(i)) * angle_incr_rad);
+    sin_ang = sinf (start_angle_rad + ((float)(i)) * angle_incr_rad);
+
+    /* Get the X and Y for inner arc of slice[i] */
+    inner_x = -inner_r * cos_ang + x;
+    inner_y = inner_r * sin_ang + y;
+
+    /* Get the X and Y for outer arc of slice[i] */
+    outer_x = -outer_r * cos_ang + x;
+    outer_y = outer_r * sin_ang + y;
+
+    /* Triangle Layout Diagram             */
+    /*    inner(x,y)                       */
+    /*     ________                        */
+    /*    |       /| outer(x,y)            */
+    /*    |      / |                       */
+    /*    |     /  |                       */
+    /*    |    /   |                       */
+    /*    |   /    |  1 Slice              */
+    /*    |  /     |                       */
+    /*    | /      |                       */
+    /*    |/_______| last_outer(x,y)       */
+    /*    last_inner(x,y)_                 */
+
+    /* Add lower triangle */
+    add_triangle ( &buffer,
+                   last_inner_x, last_inner_y,
+                   last_outer_x, last_outer_y,
+                   outer_x, outer_y
+                 );
+
+    /* Add upper triangle */
+    add_triangle ( &buffer,
+                   last_inner_x, last_inner_y,
+                   inner_x, inner_y,
+                   outer_x, outer_y
+                 );
+
+    /* Save current points for next iteration */
+    last_inner_x = inner_x;
+    last_inner_y = inner_y;
+    last_outer_x = outer_x;
+    last_outer_y = outer_y;
+    }
+
+/* Don't bother capping hair line widths */
+if( !hairline )
+    {
+  /* Draw a semicircle cap at the start */
+    draw_cap( width,
+              x + rx * -cosf (start_angle_rad),
+              y + rx *  sinf (start_angle_rad),
+              start_angle, scale
+            );
+
+    /* Draw a semicircle cap at the end */
+    draw_cap ( width,
+               x + rx * -cosf (start_angle_rad + delta_angle_rad),
+               y + rx *  sinf (start_angle_rad + delta_angle_rad),
+               start_angle + delta_angle + 180., scale
+             );
+    }
+} /* QtHID::draw_arc() */
+
+/**
+ * @Brief Flushed the triangle buffer to the openGL system so they can be rendered.
+ *
+ * @param Pointer an array of Triangles
+ * @return Void.
+ */
+void QtHID::flush_triangles( triangle_buffer* buffer )
+{
+/* Don't flush if the buffer is empty! */
+if (buffer->triangle_count != 0)
+    {
+    /* Set GL state to vertex array */
+    glEnableClientState (GL_VERTEX_ARRAY);
+
+    /* Point the GL system to the buffer */
+    glVertexPointer (3, GL_FLOAT, 0,buffer->triangle_array);
+
+    /* Draw the array as triangles */
+    glDrawArrays (GL_TRIANGLES, 0, buffer->triangle_count * 3);
+
+    glScalef( this->zoom, this->zoom, 0 );
+
+    /* Disable the vertex array */
+    glDisableClientState (GL_VERTEX_ARRAY);
+
+    /* Clean up the buffer tracking variables */
+    buffer->triangle_count = 0;
+    buffer->coord_comp_count = 0;
+    }
+} /* flush_triangles() */
 
 void QtHID::draw_line(int cap, Coord width, Coord x1, Coord y1, Coord x2, Coord y2, double scale)
 {
-	  double angle;
-	  double deltax, deltay, length;
-	  float wdx, wdy;
-	  int circular_caps = 0;
-	  int hairline = 0;
+      double angle;
+      double deltax, deltay, length;
+      float wdx, wdy;
+      int circular_caps = 0;
+      int hairline = 0;
 
-	  if (width == 0.0)
-	    hairline = 1;
+      if (width == 0.0)
+        hairline = 1;
 
-	  if (width < scale)
-	    width = scale;
+      if (width < scale)
+        width = scale;
 
-	  deltax = x2 - x1;
-	  deltay = y2 - y1;
+      deltax = x2 - x1;
+      deltay = y2 - y1;
 
-	  length = sqrt (deltax * deltax + deltay * deltay);
+      length = sqrt (deltax * deltax + deltay * deltay);
 
-	  if (length == 0) {
-	    /* Assume the orientation of the line is horizontal */
-	    wdx = -width / 2.;
-	    wdy = 0;
-	    length = 1.;
-	    deltax = 1.;
-	    deltay = 0.;
-	  } else {
-	    wdy = deltax * width / 2. / length;
-	    wdx = -deltay * width / 2. / length;
-	  }
+      if (length == 0) {
+        /* Assume the orientation of the line is horizontal */
+        wdx = -width / 2.;
+        wdy = 0;
+        length = 1.;
+        deltax = 1.;
+        deltay = 0.;
+      } else {
+        wdy = deltax * width / 2. / length;
+        wdx = -deltay * width / 2. / length;
+      }
 
-	  angle = -180. / M_PI * atan2 (deltay, deltax);
+      angle = -180. / M_PI * atan2 (deltay, deltax);
 
-	  switch (cap) {
-	    case Trace_Cap:
-	    case Round_Cap:
-	      circular_caps = 1;
-	      break;
+      switch (cap) {
+        case Trace_Cap:
+        case Round_Cap:
+          circular_caps = 1;
+          break;
 
-	    case Square_Cap:
-	    case Beveled_Cap:
-	      x1 -= deltax * width / 2. / length;
-	      y1 -= deltay * width / 2. / length;
-	      x2 += deltax * width / 2. / length;
-	      y2 += deltay * width / 2. / length;
-	      break;
-	  }
+        case Square_Cap:
+        case Beveled_Cap:
+          x1 -= deltax * width / 2. / length;
+          y1 -= deltay * width / 2. / length;
+          x2 += deltax * width / 2. / length;
+          y2 += deltay * width / 2. / length;
+          break;
+      }
 
-	  //hidgl_ensure_triangle_space (&buffer, 2);
-	  this->add_triangle(&buffer, x1 - wdx, y1 - wdy,
-	                               x2 - wdx, y2 - wdy,
-	                               x2 + wdx, y2 + wdy);
-	  add_triangle (&buffer, x1 - wdx, y1 - wdy,
-	                               x2 + wdx, y2 + wdy,
-	                               x1 + wdx, y1 + wdy);
+      //hidgl_ensure_triangle_space (&buffer, 2);
+      this->add_triangle(&buffer, x1 - wdx, y1 - wdy,
+                                   x2 - wdx, y2 - wdy,
+                                   x2 + wdx, y2 + wdy);
+      add_triangle (&buffer, x1 - wdx, y1 - wdy,
+                                   x2 + wdx, y2 + wdy,
+                                   x1 + wdx, y1 + wdy);
 
-//	  /* Don't bother capping hairlines */
-//	  if (circular_caps && !hairline)
-//	    {
-//	      draw_cap (width, x1, y1, angle + 90., scale);
-//	      draw_cap (width, x2, y2, angle - 90., scale);
-//	    }
+    /* Don't bother capping hairlines */
+    if (circular_caps && !hairline)	    {
+        draw_cap (width, x1, y1, angle + 90., scale);
+        draw_cap (width, x2, y2, angle - 90., scale);
+      }
 
 
 }
@@ -245,9 +536,9 @@ void QtHID::add_triangle(triangle_buffer *buffer,
         float x2, float y2,
         float x3, float y3)
 {
-	        add_triangle_3D (buffer, x1, y1, 1,
-	                                 x2, y2, 1,
-	                                 x3, y3, 1);
+            add_triangle_3D (buffer, x1, y1, 0,
+                                     x2, y2, 0,
+                                     x3, y3, 0);
 }
 
 void QtHID::add_triangle_3D (triangle_buffer *buffer,
@@ -268,15 +559,57 @@ void QtHID::add_triangle_3D (triangle_buffer *buffer,
 }
 void QtHID::mouseMoveEvent ( QMouseEvent * e )
 {
-	bool moved;
-	Coord x, y;
+    bool moved;
+    Coord x, y;
 
-	x = (Coord)e->x();
-	y = (Coord)e->y();
+    x = (Coord)e->x();
+    y = (Coord)e->y();
 
-	moved = MoveCrosshairAbsolute(x, y);
-	gui->notify_crosshair_change(true);
+    moved = MoveCrosshairAbsolute(x, y);
+    gui->notify_crosshair_change(true);
 
+}
+
+void QtHID::wheelEvent ( QWheelEvent * e )
+{
+float Glx;
+float Gly;
+
+#define    ZOOM_PER_WHEEL_CLICK        ( 0.1 )
+int delta;
+int sign;
+float zoom_change;
+
+/* Get the GL coordinate under the current mouse position before the zoom */
+Glx = ( e->x() * ppx ) - tx;
+Gly = ( e->y() * ppx ) - ty;
+
+delta = e->delta();
+sign = ( delta / abs(delta) );
+zoom_change = ZOOM_PER_WHEEL_CLICK * sign;
+
+zoom += zoom_change;
+ppx = 1 / zoom;
+
+
+
+printf("GLX: %f   GLY:  %f ", Glx, Gly );
+
+/* How much does the point move by the scaling factor */
+tx -= Glx * zoom_change;
+ty -= Gly * zoom_change;
+
+this->updateGL();
+}
+
+void QtHID::mousePressEvent( QMouseEvent* e )
+{
+printf("Button %d\n", e->button() );
+}
+
+void QtHID::keyPressEvent( QKeyEvent *e )
+{
+//TODO
 }
 
 /* This is the main repaint function */
@@ -284,28 +617,39 @@ void QtHID::paintGL(void)
 {
 BoxType region;
 
+
 glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
 //DrawMark();
 //flush_triangles(&buffer);
+glColor3f( 1.0f, 1.0f, 1.0f );
+glMatrixMode (GL_PROJECTION);
+glLoadIdentity ();
+
+glOrtho (0, this->width(), this->height(), 0, 0, 100);
+
+glMatrixMode (GL_MODELVIEW);
+
+glTranslatef( tx, ty, 0.0f );
+glScalef (zoom, zoom, 0 );
+printf("Scale %f \n", zoom );
 
 region.X1 = 0;
 region.Y1 = 0;
 region.X2 = this->width();
 region.Y2 = this->height();
 
+
 hidgl_draw_grid ( &region );
+draw_arc(6, 300, 300, 10, 10, 0, 360, 1 );
+this->draw_arc( 30, 200, 200, 40, 0, 0, 360, 1 );
+this->draw_arc( 30, 400, 200, 40, 0, 0, 360, 1 );
+this->draw_line( Round_Cap, 5, 300,300,500,300, 1 );
+//DrawAttached();
+this->flush_triangles( &buffer );
 
 Drawcrosshairs();
-
-
 }
-
-void QtHID::initializeGL()
-{
-qglClearColor(qtGreen.dark());
-}
-
 
 void QtHID::Drawcrosshairs(void)
 {
@@ -481,9 +825,9 @@ void QtHID::hidgl_draw_grid (BoxType *drawn_area)
   if (!Settings.DrawGrid)
  //   return;
 
-	  glEnable (GL_COLOR_LOGIC_OP);
-	  glLogicOp (GL_XOR);
-	  glColor3f( 1.0f, 1.0f, 1.0f );
+      glEnable (GL_COLOR_LOGIC_OP);
+      glLogicOp (GL_XOR);
+      glColor3f( 1.0f, 1.0f, 1.0f );
 
   x1 = GridFit (MAX (0, drawn_area->X1), 25/*PCB->Grid*/ , PCB->GridOffsetX);
   y1 = GridFit (MAX (0, drawn_area->Y1), 25/*PCB->Grid*/ , PCB->GridOffsetY);
@@ -547,7 +891,7 @@ Coord QtHID::GridFit (Coord x, Coord grid_spacing, Coord grid_offset)
 
 void QtHID::valueChanged(bool value)
 {
-	printf("one clicked %d\n", value);
+    printf("one clicked %d\n", value);
 }
 
 #include "qthid.moc"
--- src/hid/qt/qthid.h
+++ src/hid/qt/qthid.h
@@ -22,30 +22,31 @@
 #include <QObject>
 
 /*-----------------------------------------------
-  	        PreCompiler Defines
+            PreCompiler Defines
  ----------------------------------------------*/
 #define TRIANGLE_ARRAY_SIZE 5461
 
 /*-----------------------------------------------
-  	     Types ( For C interface )
+         Types ( For C interface )
  ----------------------------------------------*/
 typedef struct hid_gc_struct
 {
-	HID *me_pointer;
-	const char *colorname;
-	double alpha_mult;
-	Coord width;
-	int cap, join;
+  HID *me_pointer;
+  const char *colorname;
+  double alpha_mult;
+  Coord width;
+  int cap, join;
 };
 
-typedef struct {
-  float triangle_array [3 * 3 * TRIANGLE_ARRAY_SIZE];
-  unsigned int triangle_count;
-  unsigned int coord_comp_count;
-} triangle_buffer;
+typedef struct
+    {
+    float triangle_array [3 * 3 * TRIANGLE_ARRAY_SIZE];
+    unsigned int triangle_count;
+    unsigned int coord_comp_count;
+    } triangle_buffer;
 
 /*-----------------------------------------------
-  	  	      Class Definitions
+              Class Definitions
  ----------------------------------------------*/
 class Geometry;
 class Cube;
@@ -53,87 +54,85 @@ class Cube;
 class DrawPort
 {
 public:
-	int x;
-	int y;
+  int x;
+  int y;
 };
 
 class QtHID : public QGLWidget
 {
-
-
-
-	Q_OBJECT
+Q_OBJECT
 
 public:
-	/* Constructor(s) */
-	 QtHID(QWidget *Parent = 0);
+  /* Constructor(s) */
+   QtHID(QWidget *Parent = 0);
 
-	 /* Destructor */
-	~QtHID();
-	static QtHID*           pMe;
-	static HID*             pIntf;
+   /* Destructor */
+  ~QtHID();
+  static QtHID*           pMe;
+  static HID*             pIntf;
 
-	/* Other Public Functions */
-	void init_interface( HID* intf );
-	void initializeGL();
-	void flush_triangles (triangle_buffer *buffer);
-	void draw_line(int cap, Coord width, Coord x1, Coord y1, Coord x2, Coord y2, double scale);
-	void add_triangle_3D (triangle_buffer *buffer,
-	                       GLfloat x1, GLfloat y1, GLfloat z1,
-	                       GLfloat x2, GLfloat y2, GLfloat z2,
-	                       GLfloat x3, GLfloat y3, GLfloat z3);
-	void add_triangle(triangle_buffer *buffer,
-	        float x1, float y1,
-	        float x2, float y2,
-	        float x3, float y3);
-	void setcrosshairs( int x, int y );
+  /* Other Public Functions */
+  void init_interface( HID* intf );
+
+
+
+  void setcrosshairs( int x, int y );
 
 public slots:
-	void valueChanged(bool);
+  void valueChanged(bool);
 
 private:
-	/* Static variables */
+    /* Static Private Variables */
+    static HID_DRAW QtGraphics;
 
-	static HID_DRAW QtGraphics;
-	static QApplication*  	App; //The main QT Window Application
+    /* Private Variables */
+    triangle_buffer     buffer;
+    QColor 	            qtGreen;
+    DrawPort            qport;
+    Geometry *          geom;
+    Cube *              cube;
+    QGLPixelBuffer *    pbuffer;
+    float               zoom;
+    float               ppx;
+    float               tx;
+    float               ty;
 
-	triangle_buffer buffer;
-	QColor qtGreen;
-	DrawPort qport;
+    /* Static Private Member Functions */
+    static hidGC    make_gc            ( void );
+    static void     parse_arguments    ( int *argc, char ***argv );
+    static void     logger             ( const char *fmt, va_list args );
+    static void     set_color          ( hidGC gc, const char *name );
+    static void     set_line_cap       ( hidGC gc, EndCapStyle style );
+    static void     draw_a_line        ( hidGC gc, Coord x1, Coord y1, Coord x2, Coord y2 );
+    static void     update_widget      ( bool change_complete );
+    static void     set_crosshair      ( int x, int y, int action );
+    static void     set_draw_xor       ( hidGC gc, int xor_ );
+    static void     set_line_width     ( hidGC gc, Coord width );
 
-	/* Private Variables */
+    /* Private member Functions */
+    void            wheelEvent         ( QWheelEvent * e );
+    void            mouseMoveEvent     ( QMouseEvent * e );
+    void            mousePressEvent    ( QMouseEvent* e );
+    void            keyPressEvent      ( QKeyEvent *e );
 
-
-
-	/* Private Functions */
-    static hidGC    make_gc( void );
-	static void     parse_arguments( int *argc, char ***argv );
-	static void     logger( const char *fmt, va_list args );
-	static void     set_color( hidGC gc, const char *name );
-	static void     set_line_cap( hidGC gc, EndCapStyle style );
-    static void     draw_a_line( hidGC gc, Coord x1, Coord y1, Coord x2, Coord y2 );
-    static void     update_widget( bool change_complete );
-    static void     set_crosshair( int x, int y, int action );
-    static void     set_draw_xor(hidGC gc, int xor_);
-    static void     set_line_width( hidGC gc, Coord width );
-
-
-	void mouseMoveEvent ( QMouseEvent * e );
-	//void mousePressEvent( QMouseEvent* ev );
-	void paintGL(void);
-	void Drawcrosshairs(void);
-	void draw_slanted_cross (int x, int y, int z);
-	void draw_dozen_cross (int x, int y, int z);
-	void draw_right_cross (float x, float y, int z);
-	void hidgl_draw_grid (BoxType *drawn_area);
-	Coord GridFit (Coord x, Coord grid_spacing, Coord grid_offset);
-	void resizeGL(int width, int height);
-
-	Geometry *geom;
-	Cube *cube;
-	QGLPixelBuffer *pbuffer;
-
-
+    /* Drawing functions */
+    void            initializeGL       ();
+    void            paintGL            ( void );
+    bool            check_triangle_space( triangle_buffer *buffer, int count );
+    int             calc_slices        (float pix_radius, float sweep_angle );
+    void            Drawcrosshairs     ( void );
+    void            draw_arc           ( Coord width, Coord x, Coord y, Coord rx, Coord ry, Angle start_angle, Angle delta_angle, double scale );
+    void            draw_slanted_cross (int x, int y, int z );
+    void            draw_dozen_cross   ( int x, int y, int z );
+    void            draw_cap           ( Coord width, Coord x, Coord y, Angle angle, double scale );
+    void            draw_line          ( int cap, Coord width, Coord x1, Coord y1, Coord x2, Coord y2, double scale );
+    void            draw_right_cross   ( float x, float y, int z );
+    void            hidgl_draw_grid    ( BoxType *drawn_area );
+    Coord           GridFit            ( Coord x, Coord grid_spacing, Coord grid_offset );
+    void            resizeGL           ( int width, int height );
+    void            add_triangle       ( triangle_buffer *buffer, float x1, float y1, float x2, float y2, float x3, float y3 );
+    void            add_triangle_3D    ( triangle_buffer *buffer, GLfloat x1, GLfloat y1, GLfloat z1, GLfloat x2, GLfloat y2, GLfloat z2, GLfloat x3, GLfloat y3, GLfloat z3 );
+    void            flush_triangles    ( triangle_buffer *buffer );
 };
 
 
